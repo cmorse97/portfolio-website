6a8c57b5f2ffeea7e252e60fd9c8c76e15070b70
Caleb Morse <calebmorse.code@gmail.com>
2023-04-10 07:25:16 -0500

    Initialize app

---
+++ app/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Recommendation from './components/Recommendation';
+import Footer from './components/Footer';
+
+function App() {
+	return (
+		<div className='App'>
+			<Header />
+			<About />
+			<Projects />
+			<Skills />
+			<Recommendation />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
