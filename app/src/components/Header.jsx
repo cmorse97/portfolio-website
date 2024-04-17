830108cb507b5f647f07cec15a4c9f49d1c1c36c
Caleb Morse <calebmorse.code@gmail.com>
2024-04-16 22:25:04 -0500

    Initial commit

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,5 +1,43 @@
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<nav class='navbar navbar-expand-md bg-transparent'>
+			<div class='container-fluid'>
+				<a class='navbar-brand' href='#home'>
+					Caleb Morse
+				</a>
+				<button
+					class='navbar-toggler'
+					type='button'
+					data-bs-toggle='collapse'
+					data-bs-target='#navbarNavAltMarkup'
+					aria-controls='navbarNavAltMarkup'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span class='navbar-toggler-icon'></span>
+				</button>
+				<div class='collapse navbar-collapse' id='navbarNavAltMarkup'>
+					<div class='navbar-nav'>
+						<a class='nav-link active' aria-current='page' href='#home'>
+							Home
+						</a>
+						<a class='nav-link' href='#about'>
+							About
+						</a>
+						<a class='nav-link' href='#projects'>
+							Projects
+						</a>
+						<a class='nav-link' href='#skills'>
+							Skills
+						</a>
+						<a class='nav-link' href='#recommendations'>
+							Recommendations
+						</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
